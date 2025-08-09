48e1eed12e71e61291418946086c8c161e12729b
z3nt3n0 <jmz06@outlook.com>
2025-08-08 18:24:02 -0600

    feat (respository) : Query de Busqueda por Email t returnar UserDetails

--- src/main/java/mx/alura/forohub/repository/UsuarioResp.java
+++ src/main/java/mx/alura/forohub/repository/UsuarioResp.java
@@ -1,9 +1,12 @@
 package mx.alura.forohub.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import mx.alura.forohub.model.usuario.Usuario;
 
+@Repository
 public interface UsuarioResp extends JpaRepository<Usuario, Long> {
-
+    UserDetails findByEmail(String email);
 }
