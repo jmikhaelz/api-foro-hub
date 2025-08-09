4c6a037c4fae7b69228ddc8e9ccf6661733a532a
z3nt3n0 <jmz06@outlook.com>
2025-08-09 15:00:34 -0600

    feat (controller) : Acomplementacion de solicitudes HTTP (CRUD)

--- src/main/java/mx/alura/forohub/controller/CursoC.java
+++ src/main/java/mx/alura/forohub/controller/CursoC.java
@@ -9,11 +9,15 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -23,6 +27,7 @@ import mx.alura.forohub.model.curso.Curso;
 import mx.alura.forohub.model.curso.DataCurso;
 import mx.alura.forohub.model.curso.DataListCurso;
 import mx.alura.forohub.model.curso.DataListCursoModelAssembler;
+import mx.alura.forohub.model.curso.DataUpdateCurso;
 import mx.alura.forohub.repository.CursoResp;
 
 @RestController
@@ -63,5 +68,33 @@ public class CursoC {
                                 .map(m -> new DataListCurso(m.getId(), m.getNombre(), m.getCat()));
                 var page = pagedResourcesAssembler.toModel(pagina, dataListCursoModelAssembler);
                 return ResponseEntity.ok(page);
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<Object> about(@PathVariable Long id) {
+                var curso = repository.findById(id)
+                                .orElseThrow(() -> new ResponseStatusException(
+                                                HttpStatus.CONFLICT, "No existe el registro con el ID " + id));
+
+                return ResponseEntity.ok(
+                                new DataListCurso(curso.getId(), curso.getNombre(), curso.getCat()));
+        }
+
+        @PutMapping
+        @Transactional
+        public ResponseEntity<Object> actualizar(@RequestBody @Valid DataUpdateCurso data) {
+                var id_curso = repository.getReferenceById(data.id());
+                id_curso.actualizar(data);
+                return ResponseEntity.ok(new DataListCurso(id_curso.getId(), id_curso.getNombre(), id_curso.getCat()));
+        }
+
+        @DeleteMapping("/{id}")
+        @Transactional
+        public ResponseEntity<Object> eliminar(@PathVariable Long id) {
+                if (!repository.existsById(id)) {
+                        return ResponseEntity.notFound().build();
+                }
+                repository.deleteById(id);
+                return ResponseEntity.noContent().build();
         }
 }
