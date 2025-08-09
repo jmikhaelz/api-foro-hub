db352f6124abab1d09448da65f853ed5e5377eda
z3nt3n0 <jmz06@outlook.com>
2025-08-08 18:41:53 -0600

    feat (security) : Class Agregada como filtro de seguridad para autenticación con JWT

---
+++ src/main/java/mx/alura/forohub/infra/security/SecurityFilter.java
@@ -0,0 +1,46 @@
+package mx.alura.forohub.infra.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mx.alura.forohub.repository.UsuarioResp;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private UsuarioResp resp;
+    @Autowired
+    private TokenService tokenService;
+
+    @SuppressWarnings("null")
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        var tokenJWT = requestToken(request);
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject((String) tokenJWT);
+            var user = resp.findByEmail(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private Object requestToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "").trim();
+        }
+        return null;
+    }
+
+}
