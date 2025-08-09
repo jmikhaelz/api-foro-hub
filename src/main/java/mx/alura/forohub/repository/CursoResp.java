7ea1cf6e34950a0a253134a3afde10c18532233d
z3nt3n0 <jmz06@outlook.com>
2025-08-08 22:40:22 -0600

    feat (respository) : Query para revisar si existe el mismo registro en la BD

--- src/main/java/mx/alura/forohub/repository/CursoResp.java
+++ src/main/java/mx/alura/forohub/repository/CursoResp.java
@@ -1,9 +1,9 @@
 package mx.alura.forohub.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import mx.alura.forohub.model.curso.Categoria;
 import mx.alura.forohub.model.curso.Curso;
 
 public interface CursoResp extends JpaRepository<Curso, Long> {
-
+    boolean existsByNombreAndCat(String nombre, Categoria cat);
 }
