a88ce1a811cb6e840a5815251c7fa73d5d340bad
z3nt3n0 <jmz06@outlook.com>
2025-08-09 21:57:09 -0600

    feat (repository) : Agregamos las querys para ver estado del usuario

--- src/main/java/mx/alura/forohub/repository/UsuarioResp.java
+++ src/main/java/mx/alura/forohub/repository/UsuarioResp.java
@@ -1,6 +1,8 @@
 package mx.alura.forohub.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,7 @@ import mx.alura.forohub.model.usuario.Usuario;
 @Repository
 public interface UsuarioResp extends JpaRepository<Usuario, Long> {
     UserDetails findByEmail(String email);
+
+    @Query("SELECT u.estatus FROM Usuario u WHERE u.id = :idAutor")
+    Boolean findEstatusIsActivoById(@Param("idAutor") Long idAutor);
 }
