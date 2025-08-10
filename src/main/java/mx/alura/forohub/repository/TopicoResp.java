be183a229d6de33719070c15c1dbda4dbea5d29a
z3nt3n0 <jmz06@outlook.com>
2025-08-09 21:58:11 -0600

    feat (repository) : Agreamos la query ver la existencia de topico

--- src/main/java/mx/alura/forohub/repository/TopicoResp.java
+++ src/main/java/mx/alura/forohub/repository/TopicoResp.java
@@ -1,9 +1,11 @@
 package mx.alura.forohub.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import mx.alura.forohub.model.topico.Topico;
 
 public interface TopicoResp extends JpaRepository<Topico, Long> {
-
+    @Query("SELECT t.id FROM Topico t WHERE t.titulo = :titulo")
+    String existsByTituloContaining(String titulo);
 }
