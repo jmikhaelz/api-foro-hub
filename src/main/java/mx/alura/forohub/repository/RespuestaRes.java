3b8b67f37a71d4f10bd49f791bc44aa7581e5e1b
z3nt3n0 <jmz06@outlook.com>
2025-08-10 21:50:42 -0600

    feat (respository) : Query para enviar todos los que aun no tiene solucion Respuesta

--- src/main/java/mx/alura/forohub/repository/RespuestaRes.java
+++ src/main/java/mx/alura/forohub/repository/RespuestaRes.java
@@ -1,8 +1,13 @@
 package mx.alura.forohub.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import mx.alura.forohub.model.respuesta.Respuesta;
 
 public interface RespuestaRes extends JpaRepository<Respuesta, Long> {
+    @EntityGraph(attributePaths = { "autor", "topico" })
+    Page<Respuesta> findAllBySolucionTrue(Pageable paginacion);
 }
