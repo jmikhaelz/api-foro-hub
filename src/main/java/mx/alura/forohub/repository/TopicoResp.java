4c483782bac0a26345a78191080a10a91ff774ae
z3nt3n0 <jmz06@outlook.com>
2025-08-10 19:14:17 -0600

    feat (model) : Query sobre Topico esten abierto y mandar acceso de los otras tablas

--- src/main/java/mx/alura/forohub/repository/TopicoResp.java
+++ src/main/java/mx/alura/forohub/repository/TopicoResp.java
@@ -1,11 +1,19 @@
 package mx.alura.forohub.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import mx.alura.forohub.model.topico.Topico;
 
 public interface TopicoResp extends JpaRepository<Topico, Long> {
+
+    @EntityGraph(attributePaths = { "autor", "curso" })
+    @Query("SELECT t FROM Topico t WHERE t.estatus = 'abierto'")
+    Page<Topico> findAllByEstatusAbierto(Pageable paginacion);
+
     @Query("SELECT t.id FROM Topico t WHERE t.titulo = :titulo")
     String existsByTituloContaining(String titulo);
 }
